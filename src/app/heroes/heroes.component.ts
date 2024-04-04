a063cad416d9c15f7e791fffaffdc2c820d94a91
SilverFishSeeker <silverfishseeker@gmail.com>
2024-04-04 16:07:13 +0200

    paso 3: hero-detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,11 +3,14 @@ import { Hero } from '../hero';
 import { UpperCasePipe,NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe,FormsModule,NgFor,NgIf],
+  imports: [UpperCasePipe,FormsModule,NgFor,NgIf,HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
