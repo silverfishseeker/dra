55f9bfa99f5a15a14a133b337203a7388f902546
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2024-04-09 18:23:37 +0200

    tutorial springboot

---
+++ back/src/main/java/es/ual/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package es.ual.demo.repository;
+
+import es.ual.demo.entity.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
