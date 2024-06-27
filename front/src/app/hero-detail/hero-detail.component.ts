79b633d2bcf56f7238ffdf14215f390e183bb2ec
silverfishseeker <83122444+silverfishseeker@users.noreply.github.com>
2024-06-27 23:01:03 +0200

    Paso 5 completo

--- front/src/app/hero-detail/hero-detail.component.ts
+++ front/src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input  } from '@angular/core';
-import { NgIf, UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgIf, UpperCasePipe, Location } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,5 +13,25 @@ import { Hero } from '../hero';
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- front/src/app/heroes/heroes.component.ts
+++ front/src/app/heroes/heroes.component.ts
@@ -1,33 +1,25 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe,NgFor, NgIf } from '@angular/common';
+import { UpperCasePipe,NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe,FormsModule,NgFor,NgIf,HeroDetailComponent],
+  imports: [UpperCasePipe,FormsModule,NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
